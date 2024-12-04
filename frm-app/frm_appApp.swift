964e593956136a724cf4c245e1861dcff26d4605
Tony Nguyen <tnguyen411@gmail.com>
2024-12-04 10:56:03 -0800

    Initial Commit

---
+++ frm-app/frm_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  frm_appApp.swift
+//  frm-app
+//
+//  Created by Tony Nguyen on 2024-12-04.
+//
+
+import SwiftUI
+
+@main
+struct frm_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frm-appTests/frm_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  frm_appTests.swift
+//  frm-appTests
+//
+//  Created by Tony Nguyen on 2024-12-04.
+//
+
+import Testing
+@testable import frm_app
+
+struct frm_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
